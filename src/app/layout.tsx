b1f0d1c654c5c3c8cc280f9883af1e750126d26c
icoldplayer <unknown.com@mail.com>
2023-07-30 05:56:47 +0700

    adding meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,39 @@
-import "globals.css";
-import { TopNavBar } from "components/TopNavBar";
-import { Analytics } from "@vercel/analytics/react";
-import Footer from "components/Footer"; // Note: Import without curly braces for default exports
-
+// layout.tsx
+import 'globals.css';
+import { TopNavBar } from 'components/TopNavBar';
+import { Analytics } from '@vercel/analytics/react';
+import Footer from 'components/Footer'; // Note: Import without curly braces for default exports
 
 export const metadata = {
-  title: "ResumeHQ - Online Resume Maker",
-  description:
-    "ResumeHQ - Buat resume online gratis gak pake ribet!",
+  title: 'ResumeHQ - Online Resume Maker',
+  description: 'Buat Resume Standard ATS GRATIS! Tes Screening Resume Online GRATIS!',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const siteUrl = 'https://resume.lokerhq.com';
+  const imageUrl = `${siteUrl}/public/hq.png`;
+
   return (
     <html lang="en-id">
-      <script defer data-domain="resume.lokerhq.com" src="https://analytics.istock.id/js/script.js"></script>
+      <head>
+      <script
+        defer
+        data-domain="resume.lokerhq.com"
+        src="https://analytics.istock.id/js/script.js"
+      ></script>
+
+      <meta property="og:title" content={metadata.title} />
+      <meta property="og:description" content={metadata.description} />
+      <meta property="og:image" content={imageUrl} />
+      <meta property="og:url" content={siteUrl} />
+      <meta property="og:type" content="website" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={metadata.title} />
+      <meta name="twitter:description" content={metadata.description} />
+      <meta name="twitter:image" content={imageUrl} />
+
+      </head>
       <body>
         <TopNavBar />
         {children}
@@ -27,3 +43,36 @@ export default function RootLayout({
     </html>
   );
 }
+
+
+// import "globals.css";
+// import PageHead from 'components/Head';
+// import { TopNavBar } from "components/TopNavBar";
+// import { Analytics } from "@vercel/analytics/react";
+// import Footer from "components/Footer"; // Note: Import without curly braces for default exports
+
+
+// export const metadata = {
+//   title: "ResumeHQ - Online Resume Maker",
+//   description:
+//     "ResumeHQ - Buat resume online gratis gak pake ribet!",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en-id">
+//       <PageHead />
+//       <script defer data-domain="resume.lokerhq.com" src="https://analytics.istock.id/js/script.js"></script>
+//       <body>
+//         <TopNavBar />
+//         {children}
+//         <Analytics />
+//         <Footer />
+//       </body>
+//     </html>
+//   );
+// }
